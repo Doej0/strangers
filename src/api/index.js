6f97286ca6a9f2090a6497b04f807b65c8a94b0f
Victoria <veejo90@gmail.com>
2022-03-22 09:03:28 -0400

    small edits

--- src/api/index.js
+++ src/api/index.js
@@ -1,151 +1,147 @@
-
+//request needed from strangers things API
 
 const baseURL = "https://strangers-things.herokuapp.com";
 const COHORT_NAME = "2112-FTB-ET-WEB-PT";
-let POST_ID = "";
-
-export const setPostId = (postId) => {
-    POST_ID = postId;
-}
 
 export const registerUser = async (userObject) => {
-    const url = `${baseURL}/api/${COHORT_NAME}/users/register`;
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userObject),
-      });
-    
-      console.log(response);
-    
-      const json = await response.json();
-      console.log(json);
-    
-      localStorage.setItem("stranger_things_JWT", json.data.token);
-    
-      return json;
-}
+  const url = `${baseURL}/api/${COHORT_NAME}/users/register`;
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userObject),
+  });
+
+  console.log(response);
+
+  const json = await response.json();
+  console.log(json);
+
+  localStorage.setItem("stranger_things_JWT", json.data.token);
+
+  return json;
+};
 
 export const loginUser = async (userObject) => {
-    const url = `${baseURL}/api/${COHORT_NAME}/users/login`;
+  const url = `${baseURL}/api/${COHORT_NAME}/users/login`;
 
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userObject),
-      });
-    
-      const json = await response.json();
-      console.log(json);
-    
-      localStorage.setItem("stranger_things_JWT", json.data.token);
-}
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userObject),
+  });
+
+  const json = await response.json();
+  console.log(json);
+
+  localStorage.setItem("stranger_things_JWT", json.data.token);
+};
 
 export const getMe = async () => {
-    const url = `${baseURL}/api/${COHORT_NAME}/users/me`;
-    const response = await fetch(url);
-  
-    const json = await response.json();
-    return json;
-  };
-  
-  export const testMe = async () => {
-    // URL that we're gonna reach out to
-    const url = `${baseURL}/api/${COHORT_NAME}/test/me`;
-    const token = localStorage.getItem("stranger_things_JWT");
-  
-    const response = await fetch(url, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    console.log(response);
-  
-    //Take the body we got back and convert it to JS Object
-    const json = await response.json();
-    console.log(json);
-  
-    return json.success;
-  };
+  const url = `${baseURL}/api/${COHORT_NAME}/users/me`;
+  const response = await fetch(url);
+
+  const json = await response.json();
+  return json;
+};
+
+export const testMe = async () => {
+  // URL that we're gonna reach out to
+  const url = `${baseURL}/api/${COHORT_NAME}/test/me`;
+  const token = localStorage.getItem("stranger_things_JWT");
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  console.log(response);
+
+  //Take the body we got back and convert it to JS Object
+  const json = await response.json();
+  console.log(json);
+
+  return json.success;
+};
 
 export const getPosts = async () => {
-    // URL that we're gonna reach out to
-    const url = `${baseURL}/api/${COHORT_NAME}/posts`;
-  
-    // Grab the body given back by the API
-    const response = await fetch(url);
-    console.log(response);
-  
-    // Take the body we got back and convert it to JS Object
-    const json = await response.json();
-    console.log(json);
-  
-    return json;
-  };
+  // URL that we're gonna reach out to
+  const url = `${baseURL}/api/${COHORT_NAME}/posts`;
+
+  // Grab the body given back by the API
+  const response = await fetch(url);
+  console.log(response);
+
+  // Take the body we got back and convert it to JS Object
+  const json = await response.json();
+  console.log(json);
+
+  return json;
+};
 
 export const newPost = async (postObject) => {
-    const url = `${baseURL}/api/${COHORT_NAME}/posts`;
-    const token = localStorage.getItem("stranger_things_JWT");
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        },
-        body: JSON.stringify(postObject)
-    });
-    console.log(response);
-        
-        const json = await response.json();
-        return json;
-}
+  const url = `${baseURL}/api/${COHORT_NAME}/posts`;
+  const token = localStorage.getItem("stranger_things_JWT");
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(postObject),
+  });
+  console.log(response);
+
+  const json = await response.json();
+  return json;
+};
 
 export const editPost = async (newPost) => {
-    const url = `${baseURL}/api/${COHORT_NAME}/posts`;
-    const response = await fetch(url, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newPost),
-    });
-  
-    const json = await response.json();
-    console.log(json);
-    return json;
-  };
-  
-  export const deletePostById = async (postId) => {
-    const url = `${baseURL}/api/${COHORT_NAME}/${postId}`;
-    const token = localStorage.getItem("stranger_things_JWT");
-    const response = await fetch(url, {
-        method: "DELETE",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        },
-    });
-  
-    const json = await response.json();
-    console.log(json);
-    return json;
-  };
-  
-  export const newMessage = async (postId, userObject) => {
-    const url = `${baseURL}/api/${COHORT_NAME}/posts/${postId}/messages`;
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userObject),
-    });
-  
-    const json = await response.json();
-    return json;
-  };
+  const url = `${baseURL}/api/${COHORT_NAME}/posts`;
+  const response = await fetch(url, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newPost),
+  });
+
+  const json = await response.json();
+  console.log(json);
+  return json;
+};
+
+export const deletePostById = async (postId) => {
+  const url = `${baseURL}/api/${COHORT_NAME}/${postId}`;
+  const token = localStorage.getItem("stranger_things_JWT");
+  const response = await fetch(url, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const json = await response.json();
+  console.log(json);
+  return json;
+};
+
+export const newMessage = async (postId, userObject) => {
+  const url = `${baseURL}/api/${COHORT_NAME}/posts/${postId}/messages`;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userObject),
+  });
+
+  const json = await response.json();
+  return json;
+};
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,11 +1,19 @@
-import React from "react";
-import Message  from "./Message";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from "react";
+import Message from "./Message";
+
 const Profile = () => {
-    return (
-        <>
-            <Message />
-        </>
-    )
-}
+  return (
+    <>
+      <Message />
+      <div>
+        <h1>Messages Received</h1>
+      </div>
+      <div>
+        <h2>Sent Messages</h2>
+      </div>
+    </>
+  );
+};
 
 export default Profile;
